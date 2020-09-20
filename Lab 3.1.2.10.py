46accbda21a6a260116ede6501cceeee1e9f3304
ehab1980 <71230316+ehab1980@users.noreply.github.com>
2020-09-21 02:18:50 +0300

    Add files via upload

---
+++ Lab 3.1.2.10.py
@@ -0,0 +1,18 @@
+userWord = input('Please Enter The word: ')
+print(userWord)
+userWord = userWord.upper()
+print(userWord)
+for letter in userWord:
+    if letter == 'A':
+        continue
+    elif letter == 'E':
+        continue
+    elif letter == 'I':
+        continue
+    elif letter == 'O':
+        continue
+    elif letter == 'U':
+        continue
+    else:
+        print(letter)
+    
